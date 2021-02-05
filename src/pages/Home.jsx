9b12b2e0a505d1c78dc5050624427ff96d99e196
cuneydbolukoglu <cuneydbolukogluu@gmail.com>
2021-02-06 00:18:23 +0300

    class tag editing on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ const Home = props => {
     return (
         <section className="home-section">
             <article>
-                <h1 class="glitch" data-text="CÜNEYD">CÜNEYD</h1>
-                <h1 class="glitch text" data-text="BÖLÜKOĞLU">BÖLÜKOĞLU</h1>
+                <h1 className="glitch" data-text="CÜNEYD">CÜNEYD</h1>
+                <h1 className="glitch text" data-text="BÖLÜKOĞLU">BÖLÜKOĞLU</h1>
                 <h1>FRONT END DEVELOPER</h1>
             </article>
         </section>
