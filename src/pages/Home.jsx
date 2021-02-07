92d97379ac712fc44157fbf4f10e14cd00168dec
cuneydbolukoglu <cuneydbolukogluu@gmail.com>
2021-02-07 20:02:39 +0300

    glitch effect editing on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = props => {
             <article>
                 <h1 className="glitch" data-text="CÜNEYD">CÜNEYD</h1>
                 <h1 className="glitch text" data-text="BÖLÜKOĞLU">BÖLÜKOĞLU</h1>
-                <h1>FRONT END DEVELOPER</h1>
+                <h1>Front-End Developer</h1>
             </article>
         </section>
     )
