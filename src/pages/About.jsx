01b5747032fc19c1b5612af496153f1fc76beb38
cuneydbolukoglu <cuneydbolukogluu@gmail.com>
2021-03-17 20:05:07 +0300

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,16 @@
-import img from '../assets/img/do-more-y3.jpg';
-
 const About = props => {
     return (
         <section className="about">
             <figure>
-                {/* <img src={img} alt="img"/> */}
+                {/* <img src={img} style={{ width: '440px' }} alt="img" /> */}
             </figure>
             <article>
                 <h2>About Me.</h2>
                 <p>Hi, I'm Cüneyd. I am a <b>Front-end developer</b>,
-                        I create great sites and web applications with HTML and CSS and decorate them with <b>Javascript</b>.</p>
+                I create great sites and web applications with HTML and CSS and decorate them with <b>Javascript</b>.</p>
                 <p>Photoshop, Sketch, Adobe XD, Figma, Zeplin etc.
                 I create responsive and mobile compatible products by using BOOTSTRAP technologies.
-                        You can follow my works on <a className="link_a" href="http://gihtub.com/cuneydbolukoglu">GitHub</a> and <a className="link_a" href="http://codepen.io/cuneyd">CodePen</a>.</p>
-
+                You can follow my works on <a className="link_a" href="http://gihtub.com/cuneydbolukoglu">GitHub</a> and <a className="link_a" href="http://codepen.io/cuneyd">CodePen</a>.</p>
                 <p>If you have anything to ask, you can <a className="link_a" href="mailto:cuneydbolukogluu@gmail.com">contact me</a>.</p>
                 <p>Follow me on <a className="link_a" href="http://cuneyd.medium.com">medium</a>, <a className="link_a" href="http://www.linkedin.com/in/cuneydbolukoglu">linkedin</a>, <a className="link_a" href="https://github.com/cuneydbolukoglu">github</a></p>
             </article>
