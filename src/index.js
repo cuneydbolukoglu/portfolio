9e3e9849978d366e1d32c4fd90fcb170031dd183
cuneyd.bolukoglu <cuneydbolukogluu@gmail.com>
2021-05-02 17:29:43 +0300

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './style/icon-style.css'
 import './style/main.scss';
-import './style/glicth-effect.scss'
 
 ReactDOM.render(
   <React.StrictMode>
