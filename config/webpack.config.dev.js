752f6094da4ecd9ed24d72a8cb7d169b5b7061f9
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2024-06-28 07:30:22 +0300

    update

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const appPackageJson = require('../package.json');
+const appHtml = resolveApp('./public/index.html');
+const { proxy, menuName } = appPackageJson;
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    entry: path.resolve(__dirname, '../src/index.js'),
+    output: {
+        path: path.resolve(__dirname, '../build'),
+        filename: '[name].[contenthash].js',
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(scss|css)$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                    'postcss-loader'
+                ],
+            },
+            {
+                test: /\.(ico|gif|png|jpg|jpeg|svg)$/i,
+                loader: 'url-loader',
+                options: {
+                    limit: 8192, // 8KB'den küçük dosyalar base64 kodu olarak içe aktarılacak
+                    name: 'assets/images/[name].[ext]',
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                loader: 'url-loader',
+                options: {
+                    limit: 50000,
+                    mimeType: 'application/font-ttf',
+                    name: 'assets/fonts/[name].[ext]',
+                },
+            },
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[hash].[ext]',
+                            outputPath: 'media/',
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.PUBLIC_URL': JSON.stringify(''),
+            'process.env.MENU_NAME': JSON.stringify(menuName),
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, appHtml),
+            filename: 'index.html',
+            favicon: './public/favicon.ico',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '../public'),
+        },
+        port: 3000,
+        open: true,
+        host: 'localhost',
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+        proxy: [{
+            context: ['/api'],
+            target: proxy,
+        }],
+    },
+};
---
+++ config/webpack.config.prod.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const appPackageJson = require('../package.json');
+const appHtml = resolveApp('./public/index.html');
+const { homepage, menuName } = appPackageJson;
+
+module.exports = {
+    mode: 'production',
+    // devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, '../build'),
+        filename: 'assets/js/bundle.[contenthash].js',
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /.s?css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(ico|gif|png|jpg|jpeg|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            limit: 8192, // 8KB'den küçük dosyalar base64 kodu olarak içe aktarılacak
+                            name: 'assets/images/[name].[hash:8].[ext]',
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[hash].[ext]',
+                            outputPath: 'media/',
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin()
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'assets/css/styles.[contenthash].css'
+        }),
+        new webpack.DefinePlugin({
+            'process.env.PUBLIC_URL': JSON.stringify(homepage ? homepage : ''),
+            'process.env.MENU_NAME': JSON.stringify(menuName),
+        }),
+        new HtmlWebpackPlugin({
+            template: appHtml,
+            filename: 'index.html',
+            favicon: './public/favicon.ico',
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true,
+            },
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public', to: '', globOptions: { ignore: ['**/index.html'] } }
+            ]
+        }),
+    ]
+};
